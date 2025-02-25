922ff33a9edee1af34d3e18ea90840cc04f163e9
Elizeu Lopes <scarabellot.lopes@gmail.com>
2025-02-25 16:36:07 -0300

    feat: add express server configuration and statistical file route

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import cors from "cors";
+import path from "path";
+import routes from "./routes";
+
+const port = process.env.PORT;
+
+const app = express();
+app.use(routes);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "uploads")));
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({
+      message: err.message,
+      stack: err.stack,
+    });
+    return;
+  }
+
+  res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+    stack: err.stack,
+  });
+  return;
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}`);
+});
