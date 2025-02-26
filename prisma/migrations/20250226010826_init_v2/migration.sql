4fa1907b038acb5db8271f0246ff555e01462565
Elizeu Lopes <scarabellot.lopes@gmail.com>
2025-02-25 23:58:46 -0300

    update: add fields created and updated in the database

---
+++ prisma/migrations/20250226010826_init_v2/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `categories` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `posts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `categories` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `posts` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
